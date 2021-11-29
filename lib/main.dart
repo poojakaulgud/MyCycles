a51523b2f1c76dc70e1ea05b28d9aa53690885af
Pooja Kaulgud <pooja.kaulgud@somaiya.edu>
2021-11-30 03:31:09 +0530

    homepage version1

--- lib/main.dart
+++ lib/main.dart
@@ -1,113 +1,179 @@
+// @dart=2.9
 import 'package:flutter/material.dart';
+import 'package:table_calendar/table_calendar.dart';
 
+const magenta = const Color(0x8e3a59);
 void main() {
-  runApp(MyApp());
+  runApp(
+    MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: MyCycles(),
+    ),
+  );
 }
 
-class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
+class MyCycles extends StatefulWidget {
+  _MyCycleState createState() => _MyCycleState();
 }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
+class _MyCycleState extends State<MyCycles> {
+  CalendarController _controller;
 
   @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
+  void initState() {
+    super.initState();
+    _controller = CalendarController();
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        actions: [
+          new SizedBox(
+            height: 100.0,
+            width: 80.0,
+            child: new IconButton(
+                icon: Image.asset('assets/logo.png'), onPressed: () => {}),
+          ),
+        ],
+        title: Text(
+          "My Cycles",
+          style: TextStyle(fontFamily: 'Allura', fontSize: 30),
+        ),
+        backgroundColor: Colors.pink[900],
+        centerTitle: true,
+        elevation: 5.0,
       ),
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
+        child: new Container(
+          decoration: BoxDecoration(
+            gradient: LinearGradient(
+              begin: Alignment(-1.0, 0.0),
+              end: Alignment(1.0, 0.0),
+              stops: [
+                0.0,
+                0.1,
+                0.1,
+                0.2,
+                0.2,
+                0.3,
+                0.3,
+                0.4,
+                0.4,
+                0.5,
+                0.5,
+                0.6,
+                0.6,
+                0.7,
+                0.7,
+                0.8,
+                0.8,
+                0.9,
+                0.9,
+                1
+              ],
+              colors: [
+                Colors.pink[100],
+                Colors.pink[100],
+                Colors.pink[50],
+                Colors.pink[50],
+                Colors.pink[100],
+                Colors.pink[100],
+                Colors.pink[50],
+                Colors.pink[50],
+                Colors.pink[100],
+                Colors.pink[100],
+                Colors.pink[50],
+                Colors.pink[50],
+                Colors.pink[100],
+                Colors.pink[100],
+                Colors.pink[50],
+                Colors.pink[50],
+                Colors.pink[100],
+                Colors.pink[100],
+                Colors.pink[50],
+                Colors.pink[50],
+              ],
+              tileMode: TileMode.repeated,
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
+          ),
+          child: new Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              TableCalendar(
+                initialCalendarFormat: CalendarFormat.month,
+                calendarStyle: CalendarStyle(
+                    todayColor: Colors.blue,
+                    selectedColor: Theme.of(context).primaryColor,
+                    todayStyle: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 22.0,
+                        color: Colors.white)),
+                headerStyle: HeaderStyle(
+                  centerHeaderTitle: true,
+                  formatButtonDecoration: BoxDecoration(
+                    color: Colors.brown,
+                    borderRadius: BorderRadius.circular(22.0),
+                  ),
+                  formatButtonTextStyle: TextStyle(color: Colors.white),
+                  formatButtonShowsNext: false,
+                ),
+                startingDayOfWeek: StartingDayOfWeek.monday,
+                onDaySelected: (date, events, _) {
+                  print(date.toUtc());
+                },
+                builders: CalendarBuilders(
+                  selectedDayBuilder: (context, date, events) => Container(
+                      margin: const EdgeInsets.all(5.0),
+                      alignment: Alignment.center,
+                      decoration: BoxDecoration(
+                          color: Theme.of(context).primaryColor,
+                          borderRadius: BorderRadius.circular(8.0)),
+                      child: Text(
+                        date.day.toString(),
+                        style: TextStyle(color: Colors.white),
+                      )),
+                  todayDayBuilder: (context, date, events) => Container(
+                      margin: const EdgeInsets.all(5.0),
+                      alignment: Alignment.center,
+                      decoration: BoxDecoration(
+                          color: Colors.blue,
+                          borderRadius: BorderRadius.circular(8.0)),
+                      child: Text(
+                        date.day.toString(),
+                        style: TextStyle(color: Colors.white),
+                      )),
+                ),
+                calendarController: _controller,
+              ),
+              new Column(
+                children: [
+                  Row(
+                    children: <Widget>[
+                      Expanded(
+                          child: TextButton(
+                        onPressed: () {},
+                        child: Text("Add Period"),
+                        style: TextButton.styleFrom(
+                          primary: Colors.pink[900],
+                        ),
+                      )),
+                      Expanded(
+                        child: FloatingActionButton(
+                          onPressed: () {},
+                          child: Icon(Icons.add),
+                        ),
+                      )
+                    ],
+                  ),
+                ],
+              ),
+            ],
+          ),
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
